823fbc3dc9bf081a968969af44e25aeafa5d79ec
Francisco Di Nucci <franciscoldinucci@gmail.com>
2023-08-18 20:02:30 -0300

    foto portada

--- components/SignIn.js
+++ components/SignIn.js
@@ -11,7 +11,7 @@ function SignIn() {
             <h1 className='mb-6 text-6xl font-bold text-center'>Follow the Money!🤑</h1>
             <div className='flex flex-col overflow-hidden shadow-md shadow-slate-500 bg-slate-800 rounded-xl'>
                 <div className='h-52'>
-                    <img src="https://images.pexels.com/photos/1055081/pexels-photo-1055081.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='object-cover w-full h-full' />
+                    <img src="https://images.pexels.com/photos/730547/pexels-photo-730547.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className='object-cover w-full h-full' />
                 </div>
                 <div className='px-4 py-4'>
                     <h3 className='text-2xl text-center'>Please, sign in to continue</h3>
